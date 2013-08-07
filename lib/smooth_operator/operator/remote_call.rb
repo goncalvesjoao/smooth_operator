f0e5b86c4a971474164e73053caa0c822be4dbfd
goncalvesjoao <goncalves.joao@gmail.com>
2013-08-07 14:53:31 +0100

    remove_call class behaves like the response object

--- lib/smooth_operator/operator/remote_call.rb
+++ lib/smooth_operator/operator/remote_call.rb
@@ -15,7 +15,7 @@ module SmoothOperator
       def parse_response # TO BE OVERWRITTEN IF NECESSARY
         @raw_response
       end
-      
+
       def successful_response? # TO BE OVERWRITTEN IF NECESSARY
         @raw_response.blank? || HTTP_SUCCESS_CODES.include?(code)
       end
@@ -26,6 +26,8 @@ module SmoothOperator
 
       def ok?; successful_response?; end
 
+      def error?; !ok? && exception.present?; end
+
       def raw_response=(response)
         @raw_response = response
         @parsed_response = parse_response_and_set_exception_if_necessary
@@ -35,11 +37,37 @@ module SmoothOperator
         exception.present? ? false : @response
       end
 
+      def ==(object_to_compare)
+        response == object_to_compare
+      end
+
+      def <<(object)
+        if response.is_a? Array
+          response << object
+        else
+          response += object
+        end
+      end
+
+      def respond_to?(symbol, include_priv = false)
+        response.respond_to?(symbol, include_priv)
+      end
+
+      def to_s
+        response
+      end
+
       protected ####################### protected ##################
 
       def parse_response_and_set_exception_if_necessary
         @exception = successful_response? ? nil : SmoothOperator::Exceptions.raise_proper_exception(@raw_response, code)
         parse_response
+      end
+
+      private ####################### private ######################
+
+      def method_missing(method, *args, &block)
+        response.send(method, *args, &block)
       end
 
     end
