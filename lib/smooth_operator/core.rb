d27a366dc5880d84b6d8d8864d67a07cbeac1c9e
goncalvesjoao <goncalves.joao@gmail.com>
2013-10-08 10:50:40 +0100

    nested_objects_classes can now order a list

--- lib/smooth_operator/core.rb
+++ lib/smooth_operator/core.rb
@@ -52,37 +52,54 @@ module SmoothOperator
 
     private ######################### PRIVATE ###############################
 
-    def get_nested_object_variable(nested_object_symbol, nested_object_class)
+    def get_nested_object_variable(nested_object_symbol, nested_object_class_or_options)
       nested_object_variable = instance_variable_get("@#{nested_object_symbol}")
 
       return nested_object_variable if nested_object_variable.present?
 
-      nested_object_variable = initialize_nested_object_variable(table_hash[nested_object_symbol], nested_object_class, nested_object_symbol)
+      nested_object_variable = initialize_nested_object_variable(table_hash[nested_object_symbol], nested_object_class_or_options, nested_object_symbol)
 
       instance_variable_set("@#{nested_object_symbol}", nested_object_variable)
 
       nested_object_variable
     end
 
-    def initialize_nested_object_variable(attributes, nested_object_class, nested_object_symbol)
-      if attributes.kind_of? Array
-        attributes.map { |_attributes| get_nested_object(_attributes, nested_object_class, nested_object_symbol) }
+    def initialize_nested_object_variable(attributes, nested_object_class_or_options, nested_object_symbol)
+      if attributes.kind_of?(Array)
+        get_nested_objects(attributes, nested_object_class_or_options, nested_object_symbol)
       else
-        get_nested_object(attributes, nested_object_class, nested_object_symbol)
+        get_nested_object(attributes, nested_object_class_or_options, nested_object_symbol)
       end
     end
 
-    def get_nested_object(nested_objects_attributes, nested_object_class, nested_object_symbol)
-      if nested_objects_attributes.blank?
-        plural?(nested_object_symbol) ? [] : nil
-      else
-        nested_object_class.new(nested_objects_attributes)
+    def get_nested_objects(nested_objects_attributes, nested_object_class_or_options, nested_object_symbol)
+      nested_objects_attributes.map { |attributes| get_nested_object(_attributes, nested_object_class_or_options, nested_object_symbol) }
+
+      if nested_object_class_or_options.kind_of?(Hash)
+        nested_objects_attributes = order_by(nested_objects_attributes, nested_object_class_or_options[:order_by], nested_object_class_or_options[:order])
       end
+
+      nested_objects_attributes
+    end
+
+    def get_nested_object(nested_objects_attributes, nested_object_class_or_options, nested_object_symbol)
+      return (plural?(nested_object_symbol) ? [] : nil) if nested_objects_attributes.blank?
+      
+      nested_object_class = nested_object_class_or_options.kind_of?(Hash) ? nested_object_class_or_options[:class] : nested_object_class_or_options
+      nested_object_class.new(nested_objects_attributes) rescue nested_objects_attributes
     end
 
     def plural?(string)
       string = string.to_s
       string == string.pluralize
+    end
+
+    def order_by(list, method, order)
+      if order == :asc
+        list.sort { |a, b| a.send(method) <=> b.send(method) }
+      elsif order == :desc
+        list.sort { |a, b| b.send(method) <=> a.send(method) }
+      end
     end
 
 
@@ -93,9 +110,9 @@ module SmoothOperator
     module ClassMethods
       
       def nested_objects_classes(hash)
-        hash.each do |nested_object_symbol, nested_object_class|
+        hash.each do |nested_object_symbol, nested_object_class_or_options|
           define_method(nested_object_symbol.to_s) do
-            get_nested_object_variable(nested_object_symbol, nested_object_class)
+            get_nested_object_variable(nested_object_symbol, nested_object_class_or_options)
           end
         end
       end
--- lib/smooth_operator/version.rb
+++ lib/smooth_operator/version.rb
@@ -1,3 +1,3 @@
 module SmoothOperator
-  VERSION = "0.3.12"
+  VERSION = "0.4.00"
 end
