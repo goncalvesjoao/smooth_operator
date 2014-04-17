82ea4833f23a71eed9858392a73407d047c7c757
João Gonçalves <goncalves.joao@gmail.com>
2014-04-17 19:17:23 +0100

    delegation and model_schema tests

--- spec/smooth_operator/delegation_spec.rb
+++ spec/smooth_operator/delegation_spec.rb
@@ -3,13 +3,23 @@ require "spec_helper"
 describe SmoothOperator::Delegation do
 
   describe "#respond_to?" do
-    subject(:user) { User::Base.new(attributes_for(:user)) }
-
     context "when there is no declared schema" do
-      let(:initial_attributes_keys) { attributes_for(:user).keys.map(&:to_s) }
+      subject(:user) { User::Base.new(attributes_for(:user)) }
+      let(:initial_attributes_keys) { attributes_for(:user).keys }
 
       it 'it should return true for every attribute used uppon initialization' do
         initial_attributes_keys.each do |attribute|
+          expect(user.respond_to?(attribute)).to eq(true)
+        end
+      end
+    end
+
+    context "when there is a known schema" do
+      subject(:user) { User::WithAddressAndPosts::Son.new(attributes_for(:user)) }
+      let(:initial_attribute_and_known_schema) { attributes_for(:user).keys | [:posts, :address, :manager] }
+
+      it 'it should return true for every initialized attribute and known schema attributes' do
+        initial_attribute_and_known_schema.each do |attribute|
           expect(user.respond_to?(attribute)).to eq(true)
         end
       end
--- spec/smooth_operator/model_schema_spec.rb
+++ spec/smooth_operator/model_schema_spec.rb
@@ -3,13 +3,22 @@ require "spec_helper"
 describe SmoothOperator::ModelSchema do
 
   describe "#known_attributes" do
-    subject(:user) { User::Base.new(attributes_for(:user)) }
 
     context "when there is no declared schema" do
+      subject(:user) { User::Base.new(attributes_for(:user)) }
       let(:initial_attributes_keys) { attributes_for(:user).keys.map(&:to_s) }
 
       it 'it should reflect the attributes used uppon initialization' do
-        expect(user.known_attributes).to eq(initial_attributes_keys)
+        expect(user.known_attributes.to_a).to match_array(initial_attributes_keys)
+      end
+    end
+
+    context "when there is a known schema" do
+      subject(:user) { User::WithAddressAndPosts::Son.new(attributes_for(:user)) }
+      let(:initial_attribute_and_known_schema) { attributes_for(:user).keys.map(&:to_s) | ["posts", "address", "manager"] }
+
+      it 'it should reflect the initialized attributes and known schema attributes' do
+        expect(user.known_attributes.to_a).to match_array(initial_attribute_and_known_schema)
       end
     end
 
