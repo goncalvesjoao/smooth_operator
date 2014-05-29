3002f366ab171ad03c97f3616bd312aee0e14a45
João Gonçalves <goncalves.joao@gmail.com>
2014-05-29 12:31:26 +0100

    single_relation #persisted?

--- lib/smooth_operator/relation/single_relation.rb
+++ lib/smooth_operator/relation/single_relation.rb
@@ -2,11 +2,15 @@ module SmoothOperator
   module Relation
     class SingleRelation < ::SimpleDelegator
 
-      attr_reader :relation_name
+      attr_reader :object, :relation_name
 
       def initialize(object, relation_name)
-        @relation_name = relation_name
+        @object, @relation_name = object, relation_name
         super(object)
+      end
+
+      def persisted?
+        object.nil? ? false : object.persisted?
       end
 
     end
