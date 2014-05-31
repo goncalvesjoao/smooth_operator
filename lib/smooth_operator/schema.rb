7bf33dd9e80fa83d2eb4685e5aae210ae59f9e8a
João Gonçalves <goncalves.joao@gmail.com>
2014-06-01 00:31:12 +0100

    missing file

---
+++ lib/smooth_operator/schema.rb
@@ -0,0 +1,22 @@
+module SmoothOperator
+  module Schema
+
+    def schema(structure)
+      internal_structure.merge! Helpers.stringify_keys(structure)
+    end
+
+    def internal_structure
+      Helpers.get_instance_variable(self, :internal_structure,
+        { primary_key => nil })
+    end
+
+    def known_attribute?(attribute)
+      internal_structure.has_key?(attribute.to_s)
+    end
+
+    def attribute_type(attribute)
+      internal_structure[attribute.to_s]
+    end
+
+  end
+end
