1f7b713f8f6e6ceaf274d55dd8669b6335b77e69
João Gonçalves <goncalves.joao@gmail.com>
2014-06-04 16:50:35 +0100

    improving remote_call_specs time

--- spec/smooth_operator/remote_call_spec.rb
+++ spec/smooth_operator/remote_call_spec.rb
@@ -1,289 +1,305 @@
 require "spec_helper"
 
 describe SmoothOperator::RemoteCall do
-  subject { UserWithAddressAndPosts::Son.new(attributes_for(:user_with_address_and_posts)) }
 
   context "when the server response has a http code in the 200 range" do
-    before { subject.save(nil, { status: 200 }) }
+    before(:all) do
+      @subject = UserWithAddressAndPosts::Son.new(attributes_for(:user_with_address_and_posts))
+      @subject.save(nil, { status: 200 })
+    end
 
     it "#ok? should return true" do
-      expect(subject.last_remote_call.ok?).to be true
+      expect(@subject.last_remote_call.ok?).to be true
     end
 
     it "#not_processed? should return false" do
-      expect(subject.last_remote_call.not_processed?).to be false
+      expect(@subject.last_remote_call.not_processed?).to be false
     end
 
     it "#client_error? should return false" do
-      expect(subject.last_remote_call.client_error?).to be false
+      expect(@subject.last_remote_call.client_error?).to be false
     end
 
     it "#server_error? should return false" do
-      expect(subject.last_remote_call.server_error?).to be false
+      expect(@subject.last_remote_call.server_error?).to be false
     end
 
     it "#error? should return false" do
-      expect(subject.last_remote_call.error?).to be false
+      expect(@subject.last_remote_call.error?).to be false
     end
 
     it "#not_found? should return false" do
-      expect(subject.last_remote_call.not_found?).to be false
+      expect(@subject.last_remote_call.not_found?).to be false
     end
 
     it "#timeout? should return false" do
-      expect(subject.last_remote_call.timeout?).to be false
+      expect(@subject.last_remote_call.timeout?).to be false
     end
 
     it "#connection_failed? should return false" do
-      expect(subject.last_remote_call.connection_failed?).to be false
+      expect(@subject.last_remote_call.connection_failed?).to be false
     end
 
     it "#status should return true" do
-      expect(subject.last_remote_call.status).to be true
+      expect(@subject.last_remote_call.status).to be true
     end
   end
 
   context "when the server response has a http code in the 400 range (not 422, 404)" do
-    before { subject.save(nil, { status: 400 }) }
+    before(:all) do
+      @subject = UserWithAddressAndPosts::Son.new(attributes_for(:user_with_address_and_posts))
+      @subject.save(nil, { status: 400 })
+    end
 
     it "#ok? should return false" do
-      expect(subject.last_remote_call.ok?).to be false
+      expect(@subject.last_remote_call.ok?).to be false
     end
 
     it "#not_processed? should return false" do
-      expect(subject.last_remote_call.not_processed?).to be false
+      expect(@subject.last_remote_call.not_processed?).to be false
     end
 
     it "#client_error? should return true" do
-      expect(subject.last_remote_call.client_error?).to be true
+      expect(@subject.last_remote_call.client_error?).to be true
     end
 
     it "#server_error? should return false" do
-      expect(subject.last_remote_call.server_error?).to be false
+      expect(@subject.last_remote_call.server_error?).to be false
     end
 
     it "#error? should return true" do
-      expect(subject.last_remote_call.error?).to be true
+      expect(@subject.last_remote_call.error?).to be true
     end
 
     it "#not_found? should return false" do
-      expect(subject.last_remote_call.not_found?).to be false
+      expect(@subject.last_remote_call.not_found?).to be false
     end
 
     it "#timeout? should return false" do
-      expect(subject.last_remote_call.timeout?).to be false
+      expect(@subject.last_remote_call.timeout?).to be false
     end
 
     it "#connection_failed? should return false" do
-      expect(subject.last_remote_call.connection_failed?).to be false
+      expect(@subject.last_remote_call.connection_failed?).to be false
     end
 
     it "#status should return nil" do
-      expect(subject.last_remote_call.status).to be nil
+      expect(@subject.last_remote_call.status).to be nil
     end
   end
 
   context "when the server response has a http is 404" do
-    before { subject.save(nil, { status: 404 }) }
+    before(:all) do
+      @subject = UserWithAddressAndPosts::Son.new(attributes_for(:user_with_address_and_posts))
+      @subject.save(nil, { status: 404 })
+    end
 
     it "#ok? should return false" do
-      expect(subject.last_remote_call.ok?).to be false
+      expect(@subject.last_remote_call.ok?).to be false
     end
 
     it "#not_processed? should return false" do
-      expect(subject.last_remote_call.not_processed?).to be false
+      expect(@subject.last_remote_call.not_processed?).to be false
     end
 
     it "#client_error? should return true" do
-      expect(subject.last_remote_call.client_error?).to be true
+      expect(@subject.last_remote_call.client_error?).to be true
     end
 
     it "#server_error? should return false" do
-      expect(subject.last_remote_call.server_error?).to be false
+      expect(@subject.last_remote_call.server_error?).to be false
     end
 
     it "#error? should return true" do
-      expect(subject.last_remote_call.error?).to be true
+      expect(@subject.last_remote_call.error?).to be true
     end
 
     it "#not_found? should return true" do
-      expect(subject.last_remote_call.not_found?).to be true
+      expect(@subject.last_remote_call.not_found?).to be true
     end
 
     it "#timeout? should return false" do
-      expect(subject.last_remote_call.timeout?).to be false
+      expect(@subject.last_remote_call.timeout?).to be false
     end
 
     it "#connection_failed? should return false" do
-      expect(subject.last_remote_call.connection_failed?).to be false
+      expect(@subject.last_remote_call.connection_failed?).to be false
     end
 
     it "#status should return nil" do
-      expect(subject.last_remote_call.status).to be nil
+      expect(@subject.last_remote_call.status).to be nil
     end
   end
 
   context "when the server response has a http is 422" do
-    before { subject.save(nil, { status: 422 }) }
+    before(:all) do
+      @subject = UserWithAddressAndPosts::Son.new(attributes_for(:user_with_address_and_posts))
+      @subject.save(nil, { status: 422 })
+    end
 
     it "#ok? should return false" do
-      expect(subject.last_remote_call.ok?).to be false
+      expect(@subject.last_remote_call.ok?).to be false
     end
 
     it "#not_processed? should return true" do
-      expect(subject.last_remote_call.not_processed?).to be true
+      expect(@subject.last_remote_call.not_processed?).to be true
     end
 
     it "#client_error? should return true" do
-      expect(subject.last_remote_call.client_error?).to be true
+      expect(@subject.last_remote_call.client_error?).to be true
     end
 
     it "#server_error? should return false" do
-      expect(subject.last_remote_call.server_error?).to be false
+      expect(@subject.last_remote_call.server_error?).to be false
     end
 
     it "#error? should return false" do
-      expect(subject.last_remote_call.error?).to be false
+      expect(@subject.last_remote_call.error?).to be false
     end
 
     it "#not_found? should return false" do
-      expect(subject.last_remote_call.not_found?).to be false
+      expect(@subject.last_remote_call.not_found?).to be false
     end
 
     it "#timeout? should return false" do
-      expect(subject.last_remote_call.timeout?).to be false
+      expect(@subject.last_remote_call.timeout?).to be false
     end
 
     it "#connection_failed? should return false" do
-      expect(subject.last_remote_call.connection_failed?).to be false
+      expect(@subject.last_remote_call.connection_failed?).to be false
     end
 
     it "#status should return false" do
-      expect(subject.last_remote_call.status).to be false
+      expect(@subject.last_remote_call.status).to be false
     end
   end
 
   context "when the server response has a http code in the 500 range" do
-    before { subject.save(nil, { status: 500 }) }
+    before(:all) do
+      @subject = UserWithAddressAndPosts::Son.new(attributes_for(:user_with_address_and_posts))
+      @subject.save(nil, { status: 500 })
+    end
 
     it "#ok? should return false" do
-      expect(subject.last_remote_call.ok?).to be false
+      expect(@subject.last_remote_call.ok?).to be false
     end
 
     it "#not_processed? should return false" do
-      expect(subject.last_remote_call.not_processed?).to be false
+      expect(@subject.last_remote_call.not_processed?).to be false
     end
 
     it "#client_error? should return false" do
-      expect(subject.last_remote_call.client_error?).to be false
+      expect(@subject.last_remote_call.client_error?).to be false
     end
 
     it "#server_error? should return true" do
-      expect(subject.last_remote_call.server_error?).to be true
+      expect(@subject.last_remote_call.server_error?).to be true
     end
 
     it "#error? should return true" do
-      expect(subject.last_remote_call.error?).to be true
+      expect(@subject.last_remote_call.error?).to be true
     end
 
     it "#not_found? should return false" do
-      expect(subject.last_remote_call.not_found?).to be false
+      expect(@subject.last_remote_call.not_found?).to be false
     end
 
     it "#timeout? should return false" do
-      expect(subject.last_remote_call.timeout?).to be false
+      expect(@subject.last_remote_call.timeout?).to be false
     end
 
     it "#connection_failed? should return false" do
-      expect(subject.last_remote_call.connection_failed?).to be false
+      expect(@subject.last_remote_call.connection_failed?).to be false
     end
 
     it "#status should return nil" do
-      expect(subject.last_remote_call.status).to be nil
+      expect(@subject.last_remote_call.status).to be nil
     end
   end
 
   context "when the connection is broken" do
-    subject { User::BrokenConnection.new }
-
-    before { subject.save }
+    before(:all) do
+      @subject = User::BrokenConnection.new
+      @subject.save
+    end
 
     it "#ok? should return false" do
-      expect(subject.last_remote_call.ok?).to be false
+      expect(@subject.last_remote_call.ok?).to be false
     end
 
     it "#not_processed? should return false" do
-      expect(subject.last_remote_call.not_processed?).to be false
+      expect(@subject.last_remote_call.not_processed?).to be false
     end
 
     it "#client_error? should return false" do
-      expect(subject.last_remote_call.client_error?).to be false
+      expect(@subject.last_remote_call.client_error?).to be false
     end
 
     it "#server_error? should return true" do
-      expect(subject.last_remote_call.server_error?).to be true
+      expect(@subject.last_remote_call.server_error?).to be true
     end
 
     it "#error? should return true" do
-      expect(subject.last_remote_call.error?).to be true
+      expect(@subject.last_remote_call.error?).to be true
     end
 
     it "#not_found? should return false" do
-      expect(subject.last_remote_call.not_found?).to be false
+      expect(@subject.last_remote_call.not_found?).to be false
     end
 
     it "#timeout? should return false" do
-      expect(subject.last_remote_call.timeout?).to be false
+      expect(@subject.last_remote_call.timeout?).to be false
     end
 
     it "#connection_failed? should return true" do
-      expect(subject.last_remote_call.connection_failed?).to be true
+      expect(@subject.last_remote_call.connection_failed?).to be true
     end
 
     it "#status should return nil" do
-      expect(subject.last_remote_call.status).to be nil
+      expect(@subject.last_remote_call.status).to be nil
     end
   end
 
-  context "when the connection exceeds the timeout" do
-    subject { User::TimeoutConnection.new }
-
-    before { subject.save('/timeout') }
+  context "when the connection exceeds the timeout", current: true do
+    before(:all) do
+      @subject = User::TimeoutConnection.new
+      @subject.save('/timeout')
+    end
 
     it "#ok? should return false" do
-      expect(subject.last_remote_call.ok?).to be false
+      expect(@subject.last_remote_call.ok?).to be false
     end
 
     it "#not_processed? should return false" do
-      expect(subject.last_remote_call.not_processed?).to be false
+      expect(@subject.last_remote_call.not_processed?).to be false
     end
 
     it "#client_error? should return false" do
-      expect(subject.last_remote_call.client_error?).to be false
+      expect(@subject.last_remote_call.client_error?).to be false
     end
 
     it "#server_error? should return true" do
-      expect(subject.last_remote_call.server_error?).to be true
+      expect(@subject.last_remote_call.server_error?).to be true
     end
 
     it "#error? should return true" do
-      expect(subject.last_remote_call.error?).to be true
+      expect(@subject.last_remote_call.error?).to be true
     end
 
     it "#not_found? should return false" do
-      expect(subject.last_remote_call.not_found?).to be false
+      expect(@subject.last_remote_call.not_found?).to be false
     end
 
     it "#timeout? should return true" do
-      expect(subject.last_remote_call.timeout?).to be true
+      expect(@subject.last_remote_call.timeout?).to be true
     end
 
     it "#connection_failed? should return false" do
-      expect(subject.last_remote_call.connection_failed?).to be false
+      expect(@subject.last_remote_call.connection_failed?).to be false
     end
 
     it "#status should return nil" do
-      expect(subject.last_remote_call.status).to be nil
+      expect(@subject.last_remote_call.status).to be nil
     end
   end
 
@@ -299,20 +315,24 @@ describe SmoothOperator::RemoteCall do
 
   describe "#http_status" do
     context "when a server connection is established" do
-      before { subject.save(nil, { status: 422 }) }
+      before do
+        @subject = UserWithAddressAndPosts::Son.new(attributes_for(:user_with_address_and_posts))
+        @subject.save(nil, { status: 422 })
+      end
 
       it "it should return the server's http response code" do
-        expect(subject.last_remote_call.http_status).to be 422
+        expect(@subject.last_remote_call.http_status).to be 422
       end
     end
 
     context "when a server connection fails" do
-      subject { User::TimeoutConnection.new }
-
-      before { subject.save('/timeout') }
+      before do
+        @subject = User::TimeoutConnection.new
+        @subject.save('/timeout')
+      end
 
       it "should return 0" do
-        expect(subject.last_remote_call.http_status).to be 0
+        expect(@subject.last_remote_call.http_status).to be 0
       end
     end
   end
